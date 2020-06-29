5c29603b822fb807be71987f92dc810f23e989cf
Satyam Mishra <dev.sam1210@gmail.com>
2020-06-29 19:26:07 +0530

    Fixed Inclusion Errors

--- Not Engine/Source/Not Engine.h
+++ Not Engine/Source/Not Engine.h
@@ -1,14 +1,13 @@
 #pragma once
+/** Single Header Interface for Not Engine */
 
-/** For Use by Others */
-
-#include <Application.h>
-#include <Log.h>
-#include <Engine.h>
-#include <Scene.h>
-#include <Node.h>
-#include <Node2D.h>
-#include <Node3D.h>
-#include <Rect2D.h>
-#include <ColorRect.h>
-#include <TextureRect.h>
+#include "Application.h"
+#include "Log.h"
+#include "Engine.h"
+#include "Scene.h"
+#include "Node.h"
+#include "Node2D.h"
+#include "Node3D.h"
+#include "Rect2D.h"
+#include "ColorRect.h"
+#include "TextureRect.h"
